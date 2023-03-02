91be56ba20ffaa87a0fb3472b90efcf0de38374b
Rahul Vishwakarma <103241598+rahulvishwakarma0111@users.noreply.github.com>
2023-03-02 16:56:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// your code here
+function buttonClick() {
+	let name=document.getElementById("name").value
+    let year=document.getElementById("year").value
+	let url=document.getElementById("url")
+	
+	if (name!="" && year!="") {
+		url.innerText="https://localhost:8080/?name="+`${name}&year=${year}`
+	}
+    else if(name!=""){
+		url.innerText="https://localhost:8080/?name="+`${name}`
+	}
+    else if(year!=""){
+		url.innerText="https://localhost:8080/?year="+`${year}`
+	}
+}
